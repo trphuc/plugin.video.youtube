35d47c41957063904067cdd7186510b428ebb9cf
bromix <bromix@gmx.net>
2014-12-16 19:55:37 +0100

    FIX: added localization for 'create new playlist' (again)

--- resources/lib/youtube/provider.py
+++ resources/lib/youtube/provider.py
@@ -39,7 +39,8 @@ class Provider(kodion.AbstractProvider):
                  'youtube.sign.enter_code': 30519,
                  'youtube.video.add_to_playlist': 30520,
                  'youtube.playlist.select': 30521,
-                 'youtube.rename': 30113}
+                 'youtube.rename': 30113,
+                 'youtube.playlist.create': 30522}
 
     def __init__(self):
         kodion.AbstractProvider.__init__(self)
