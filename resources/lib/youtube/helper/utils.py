6f1a9cdc47342a62602509d3aa3f176ebb7ca53f
bromix <bromix@gmx.net>
2014-12-26 00:10:51 +0100

    resolves #90

--- resources/lib/youtube/helper/utils.py
+++ resources/lib/youtube/helper/utils.py
@@ -93,8 +93,17 @@ def update_video_infos(provider, context, video_id_dict, playlist_item_id_dict=N
                 channel_id_dict[channel_id] = []
             channel_id_dict[channel_id].append(video_item)
             pass
+
+        context_menu = []
+
+        if provider.is_logged_in():
+            # add 'Watch Later' only if we are not in my 'Watch Later' list
+            watch_later_playlist_id = my_playlists.get('watchLater', '')
+            yt_context_menu.append_watch_later(context_menu, provider, context, watch_later_playlist_id, video_id)
+            pass
+
+        # got to [CHANNEL]
         if channel_id and channel_name:
-            context_menu = []
             # only if we are not directly in the channel provide a jump to the channel
             if kodion.utils.create_path('channel', channel_id) != context.get_path():
                 yt_context_menu.append_go_to_channel(context_menu, provider, context, channel_id, channel_name)
@@ -105,10 +114,6 @@ def update_video_infos(provider, context, video_id_dict, playlist_item_id_dict=N
         yt_context_menu.append_related_videos(context_menu, provider, context, video_id)
 
         if provider.is_logged_in():
-            # add 'Watch Later' only if we are not in my 'Watch Later' list
-            watch_later_playlist_id = my_playlists.get('watchLater', '')
-            yt_context_menu.append_watch_later(context_menu, provider, context, watch_later_playlist_id, video_id)
-
             # add 'Like Video' only if we are not in my 'Liked Videos' list
             liked_videos_playlist = my_playlists.get('likes', '')
             yt_context_menu.append_like_video(context_menu, provider, context, liked_videos_playlist, video_id)
