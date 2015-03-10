ef1c681c5360582d2660cea69a5f99771455010d
bromix <bromix@gmx.net>
2015-03-10 19:48:17 +0100

    [upd] kodion for testing new version implementation

--- resources/lib/kodion/impl/xbmc/xbmc_system_version.py
+++ resources/lib/kodion/impl/xbmc/xbmc_system_version.py
@@ -10,34 +10,21 @@ class XbmcSystemVersion(AbstractSystemVersion):
         major = 0
         minor = 0
         try:
-            version_str = str(xbmc.__version__)
-            version_match = re.match('^(?:(?P<major>\d+)\.)?(?:(?P<minor>\d+)\.)?(?P<edition>\*|\d+)$', version_str)
-            if version_match:
-                major = int(version_match.group('major'))
-                minor = int(version_match.group('minor'))
-                pass
+            self._version = tuple(map(int, (xbmc.__version__.split("."))))
         except:
+            self._version = (1, 4)  # Frodo
             pass
 
-        name = 'Unknown XBMC System'
-        if major == 1 and minor >= 4:
-            name = 'Frodo'
+        self._name = 'Unknown XBMC System'
+        if self._version >= (1, 4) or self._version == (2, 0):
+            self._name = 'Frodo'
             pass
-        elif major == 2:
-            if minor == 0:
-                name = 'Frodo'
-                pass
-
-            if minor > 0:
-                name = 'Gotham'
-                pass
-
-            if minor > 14:
-                name = 'Helix'
-                pass
+        if self._version > (2, 0):
+            self._name = 'Gotham'
             pass
-
-        AbstractSystemVersion.__init__(self, major, minor, name)
+        if self._version > (2, 14):
+            self._name = 'Helix'
+            pass
         pass
 
     pass
