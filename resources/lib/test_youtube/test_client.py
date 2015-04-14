a14d900153d89554b0cdaff680a85a54656f7eac
bromix <bromix@gmx.net>
2015-04-14 20:58:45 +0200

    [upd] first steps to extract more meta informations

--- resources/lib/test_youtube/test_client.py
+++ resources/lib/test_youtube/test_client.py
@@ -233,9 +233,6 @@ class TestClient(unittest.TestCase):
         streams = client.get_video_streams(context, 'Hp0gI8KJw20')
         self.assertGreater(len(streams), 0)
 
-        streams = client.get_video_streams(context, 'ZDX8SoYMizA')
-        self.assertGreater(len(streams), 0)
-
         streams = client.get_video_streams(context, 'OSUy2uA6fbw')
         self.assertGreater(len(streams), 0)
 
